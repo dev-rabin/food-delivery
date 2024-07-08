ff00a5f731e359272de39768e149698dffb3f955
dev-rabin <robinmandhotia@gmail.com>
2024-07-08 13:24:41 +0530

    search bar completed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,30 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, StatusBar, Text, TextInput, View } from "react-native";
+import * as Icon from "react-native-feather";
+import {themeColors} from "../theme/index"
 
 export default function HomeScreen() {
   return (
     <>
-    <SafeAreaView>
-      <Text className=" text-red-500 p-5">
-        Food Delivery App
-      </Text>
-    </SafeAreaView>
+      <SafeAreaView className="bg-white p-3">
+        <StatusBar barStyle="dark-content" />
+
+        {/* Search Bar */}
+        <View className="flex-row items-center space-x-2 px-4 pb-2">
+          <View className="flex-row flex-1 items-center rounded-full border p-3 border-gray-300">
+            <Icon.Search height="25" width="25" stroke="grey" />
+            <TextInput placeholder="Restaurants" className=" ml-1 flex-1" />
+            <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-gray-400">
+              <Icon.MapPin height="21" width="21" stroke="grey" />
+              <Text>
+                Haryana, IND
+              </Text>
+            </View>
+          </View>
+          <View style={{backgroundColor : themeColors.bgColor}} className="p-3 rounded-full">
+            <Icon.Sliders height="21" width="21" strokeWidth={2.5} stroke="white"/>
+          </View>
+        </View>
+      </SafeAreaView>
     </>
   )
 }
---
+++ theme/index.js
@@ -0,0 +1,5 @@
+export const themeColors = {
+    text : "#f97316",
+    // bgColor : opacity=> `rgb(256,146,60, ${opacity})`
+    bgColor : "orange"
+}
